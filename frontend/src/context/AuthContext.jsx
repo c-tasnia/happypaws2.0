fbe85b6145f73d9b1476388fa99ed3d6c17e28c7
afrin-sultana23 <afriin474@gmail.com>
2026-04-11 13:02:12 +0600

    authContext added

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,55 @@
+import React, { createContext, useContext, useEffect, useState } from 'react'
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged
+} from 'firebase/auth'
+import { auth } from '../firebase/firebase.config'
+
+const AuthContext = createContext()
+
+
+export const useAuth = () => {
+    return useContext(AuthContext)
+}
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    const signup = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const login = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const logout = () => {
+        return signOut(auth)
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser)
+            setLoading(false)
+        })
+
+        return () => unsubscribe()
+    }, [])
+
+    const value = {
+        user,
+        signup,
+        login,
+        logout,
+        loading
+    }
+
+    return (
+        <AuthContext.Provider value={value}>
+            {!loading && children}
+        </AuthContext.Provider>
+    )
+}
